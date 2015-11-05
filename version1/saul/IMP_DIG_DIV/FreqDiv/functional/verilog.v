da08254b32618637cd1edba7e186af5ea3404077
saul <saul@localhost.localdomain>
2015-11-05 17:42:49 +0100

    corrected PLL

--- version1/saul/IMP_DIG_DIV/FreqDiv/functional/verilog.v
+++ version1/saul/IMP_DIG_DIV/FreqDiv/functional/verilog.v
@@ -17,28 +17,21 @@ output reg Fout;
 output F_PFD;
 
 //div by 2 register of size 15 (input frequency Fin will be appended so that there are 16 frecs!)
-wire [14:0] divider;
+reg [14:0] divider;
 
 wire F_PFD; // Reference for the PFD Div-by-32
 
-assign F_PFD = divider[4];
+// F_PFD is the clock signal that is compared with the reference frequency at the Phase-Freq detector
+assign F_PFD = divider[4]; 
 
-//Divider by two code {F14:F0}{Fin}
-Divby2 div1(Fin,divider[0],Resetn); //16MHz
-Divby2 div2(divider[0],divider[1],Resetn); //8MHz
-Divby2 div3(divider[1],divider[2],Resetn); //4MHz
-Divby2 div4(divider[2],divider[3],Resetn); //2MHz
-Divby2 div5(divider[3],divider[4],Resetn); //1MHz
-Divby2 div6(divider[4],divider[5],Resetn); //500kHz
-Divby2 div7(divider[5],divider[6],Resetn); //250kHz
-Divby2 div8(divider[6],divider[7],Resetn); //125KHz
-Divby2 div9(divider[7],divider[8],Resetn); //62.5kHz
-Divby2 div10(divider[8],divider[9],Resetn); //31.25 kHz
-Divby2 div11(divider[9],divider[10],Resetn); //15.625 kHz
-Divby2 div12(divider[10],divider[11],Resetn); //7.8125 kHz
-Divby2 div13(divider[11],divider[12],Resetn); //3.90625 kHz 
-Divby2 div14(divider[12],divider[13],Resetn); //1.953125 kHz
-Divby2 div15(divider[13],divider[14],Resetn); //0.9765625 kHz
+always @(posedge Fin, negedge Resetn)
+begin
+    if (Resetn == 0) begin
+        divider = 0;
+    end else begin
+        divider = divider + 1;
+    end
+end
 
 // MUX 16:1
 always @(Fsel,divider,Fin)
