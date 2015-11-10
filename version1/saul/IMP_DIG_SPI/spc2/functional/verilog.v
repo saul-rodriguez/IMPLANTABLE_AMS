738fee7e1a431c5b00ca2502919bd4b1220016b4
saul <saul@localhost.localdomain>
2015-11-10 17:42:41 +0100

    updated spc2

--- version1/saul/IMP_DIG_SPI/spc2/functional/verilog.v
+++ version1/saul/IMP_DIG_SPI/spc2/functional/verilog.v
@@ -8,7 +8,9 @@ IQ,
 GS,
 CE,
 NS,
-GD
+GD,
+FS,
+RE
 );
 
 input Cfg_in, Clk, Resetn;
@@ -19,9 +21,11 @@ output reg [3:0] GS;
 output reg CE;
 output reg NS;
 output reg [2:0] GD;
+output reg FS;
+output reg RE;
 
-reg [13:0] out; 
-reg [3:0] count;
+reg [15:0] out; 
+reg [4:0] count;
 
 wire strobe;
 
@@ -32,10 +36,12 @@ always @(posedge Clk, negedge Resetn)
 begin
     if (Resetn == 0) begin
         out <= 0;
-        count <= 14;
+        count <= 16;
     end else begin
         //Shift the data
-        out[13] <= Cfg_in;
+        out[15] <= Cfg_in;
+        out[14] <= out[15];
+        out[13] <= out[14];
         out[12] <= out[13];
         out[11] <= out[12];
         out[10] <= out[11];
@@ -66,13 +72,17 @@ begin
         CE <= 0;
         NS <= 0;
         GD[2:0] <= 0;
+        FS <= 0;
+        RE <= 0;
     end else begin
-        F[3:0] <= out[13:10];
-        IQ <= out[9];
-        GS[3:0] <= out[8:5];
-        CE <= out[4];
-        NS <= out[3];
-        GD[2:0] <= out[2:0];
+        F[3:0] <= out[15:12];
+        IQ <= out[11];
+        GS[3:0] <= out[10:7];
+        CE <= out[6];
+        NS <= out[5];
+        GD[2:0] <= out[4:2];
+        FS <= out[1];
+        RE <= out[0];
     end
 end
 
