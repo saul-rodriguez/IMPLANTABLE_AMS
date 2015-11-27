3f29726048d16c6ae61b965ee3a3bb63f84e6d3f
saul <saul_alejandro_r@hotmail.com>
2015-11-27 09:17:58 +0100

    replaced lna gm by sgen gm

--- version1/saul/init.sh
+++ version1/saul/init.sh
@@ -2,22 +2,18 @@
 # File "init.sh"
 # Cadence initialization file for AMS CMOS 0.18um
 # Author: Saul Rodriguez
-# date: 2015-05-17
+# date: 2015-11-25
 
 ###########################
 # Set path to AMS C18 PDK #
 ###########################
-export AMS_DIR=/afs/ict.kth.se/proj/ektlab/PDK/AMS
+export AMS_DIR=/pkg/AMS411
 export PATH=$PATH:$AMS_DIR/cds/bin:$AMS_DIR/programs/bin
-#export PATH=$PATH:$AMS_DIR/cds/bin:$AMS_DIR/programs/bin
-
-#export CDSHOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/IC615_500_171
-#export PATH=$PATH:$CDSHOME/tools/bin:$CDSHOME/tools/dfII/bin
 
 ################################
 # Set path to Cadence Virtuoso #
 ################################ 
-export CDSDIR=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/IC615_500_171
+export CDSDIR=/pkg/Cadence/installs/IC615
 export PATH=$PATH:$CDSDIR/tools/bin:$CDSDIR/tools/dfII/bin
 export CDS_AUTO_64BIT=ALL
 # These lines will enable some modgen options (6.1.5)
@@ -31,35 +27,28 @@ export CDS_Netlisting_Mode="Analog"
 ##################################
 # Set path to MMSIM (simulators) #
 ##################################
-#export MMSIM_HOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/MMSIM101
-#export MMSIM_HOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/MMSIM121
-export MMSIM_HOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/MMSIM141
+export MMSIM_HOME=/pkg/Cadence/installs/MMSIM141
 export PATH=$PATH:$MMSIM_HOME/tools/bin
 export CDS_AHDLCMI_ENABLE=NO
 
 ############################
 # Set ASSURA DRC/LVS tools #
 ############################
-export ASSURAHOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/ASSURA41
+export ASSURAHOME=/pkg/Cadence/installs/ASSURA41
 export PATH=$PATH:$ASSURAHOME/tools/bin:$ASSURAHOME/tools/assura/bin
 export ASSURA_AUTO_64BIT=ALL
 
 ################################
 # Set QRC Parasitic Extraction #
 ################################
-#export QRC_HOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/EXT91
-export QRC_HOME=/afs/kth.se/pkg/designkits/ekt/cdc/installs/EXT142
+export QRC_HOME=/pkg/Cadence/installs/EXT142
 export PATH=$PATH:$QRC_HOME/tools/bin
 export QRC_ENABLE_EXTRACTION="t"
 
 ######################
 # Set INCISIVE tools #
 ######################
-#export IUSDIR=/nobackup/saul/pkg/INCISIV121
-#export IUSDIR=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/INCISIVE142
-export IUSDIR=/afs/kth.se/pkg/designkits/ekt/cdc/installs/INCISIVE142
-#export INSISIVE=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/INCISIVE142
-#export IUSDIR=/afs/ict.kth.se/pkg/cadence/incisiv141/14.10.004
+export IUSDIR=/pkg/Cadence/installs/INCISIVE142
 export PATH=$PATH:$IUSDIR/tools/bin/64bit:$IUSDIR/tools/bin
 
 export CDS_BIND_TMP_DD=true
@@ -67,14 +56,14 @@ export CDS_BIND_TMP_DD=true
 ##########################################
 # ENCOUNTER DIGITAL IMPLEMENTATION (EDI) #
 ##########################################
-export EDIHOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/EDI141
+export EDIHOME=/pkg/Cadence/installs/EDI142
 export PATH=$PATH:$EDIHOME/tools/bin/64bit:$EDIHOME/tools/bin
 export CDS_ENABLE_EXP_PCELL=TRUE
 
 ###################
 # RC RTL Compiler #
 ###################
-export RTL_HOME=/afs/ict.kth.se/pkg/designkits/ekt/cdc/installs/RC141
+export RTL_HOME=/pkg/Cadence/installs/RC142
 export PATH=$PATH:$RTL_HOME/tools.lnx86/bin
 
 
@@ -86,6 +75,7 @@ export LM_LICENSE_FILE=@license.it.kth.se
 echo Cadence Environemnt initalization for AMS C18/H18 completed 
 echo to create a C18/a18 project or run virtuoso
 echo uncomment one of the lines in the script start_ams.sh and run it  
+
 
 
 
